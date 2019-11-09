a4b463c1d1974453ac22359c70676570036c9723
riky126 <55888923+riky126@users.noreply.github.com>
2019-11-09 13:20:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
 <head>
-    <titile>Hello World</titile>
+    <titile>Hello World!</titile>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
     <h2>Some H2 tag</h2>
 </body>
 </html>
